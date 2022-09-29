2f9b6e65a88971ada084eebe490696ce77c282a3
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2022-09-28 21:52:57 -0700

    rapid API music fetching

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,7 +1,9 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants'
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 const Discover = () => {
     const genreTitle = 'Pop';
+    const { data, isFetching, error } = useGetTopChartsQuery();
 
     return (
         <div className='flex flex-col'>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,1 +1,24 @@
 // main file for making API calls to shazam core
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'; 
+
+    
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
+        prepareHeaders:
+            (headers) => {
+                headers.set('X-RapidAPI-Key', '0695ca0183mshb9416d8acb30621p17dfd8jsne2b3e84304a2');
+                return headers;
+            }
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: (genre)=>
+             '/charts/world'}),
+    }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi; 
+
+    
+    
